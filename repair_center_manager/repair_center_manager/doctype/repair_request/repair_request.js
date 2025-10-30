1e9fe1a552744badd7641499504a57d0b2bc788c
Houssam <Eng.houssam.sawan@gmail.com>
2025-10-30 12:34:12 +0000

    add service center docktype

--- repair_center_manager/repair_center_manager/doctype/repair_request/repair_request.js
+++ repair_center_manager/repair_center_manager/doctype/repair_request/repair_request.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Houssam Sawan and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Repair Request", {
-// 	refresh(frm) {
+frappe.ui.form.on("Repair Request", {
+ 	refresh: function(frm) {
+        frm.set_query("customer_contact", function (doc) {
+			return {
+				filters: {
+					//status: "Active",
+                    }
+			};
+		});
 
-// 	},
-// });
+ 	},
+ });
--- repair_center_manager/repair_center_manager/doctype/repair_request/repair_request.py
+++ repair_center_manager/repair_center_manager/doctype/repair_request/repair_request.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, Houssam Sawan and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _, msgprint, throw
 from frappe.model.document import Document
 
 
 class RepairRequest(Document):
-	pass
+	def validate(self):
+		self.test_validate()
+
+	def test_validate(self):
+
+		if self.snimei == "12345":
+			frappe.throw(_("Invalid SN/IMEI number."))
+		
