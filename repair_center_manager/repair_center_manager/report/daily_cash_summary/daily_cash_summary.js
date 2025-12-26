98730d877badc5aa24bf972a38bda58ce5cc84e1
Houssam <Eng.houssam.sawan@gmail.com>
2025-12-26 04:04:03 +0000

    handel reports

--- repair_center_manager/repair_center_manager/report/daily_cash_summary/daily_cash_summary.js
+++ repair_center_manager/repair_center_manager/report/daily_cash_summary/daily_cash_summary.js
@@ -1,8 +1,24 @@
-// Copyright (c) 2025, Houssam Sawan and contributors
-// For license information, please see license.txt
-
 frappe.query_reports["Daily Cash Summary"] = {
-	"filters": [
-
-	]
+    filters: [
+        {
+            fieldname: "from_date",
+            label: "From Date",
+            fieldtype: "Date",
+            reqd: 1,
+            default: frappe.datetime.get_today()
+        },
+        {
+            fieldname: "to_date",
+            label: "To Date",
+            fieldtype: "Date",
+            reqd: 1,
+            default: frappe.datetime.get_today()
+        },
+        {
+            fieldname: "service_center_filter",
+            label: "Service Center",
+            fieldtype: "Link",
+            options: "Service Center"
+        }
+    ]
 };
